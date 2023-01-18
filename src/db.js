65d65dedafe1eec764b89b5519a201853068a9bd
Paula Kunning <paulakunning@gmail.com>
2023-01-18 16:52:14 -0300

    deploy

--- src/db.js
+++ src/db.js
@@ -14,11 +14,6 @@ const {
 const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialectOptions: {
-    ssl: {
-      require: true,
-    }
-  }
 });
 
 
