3ae0a5d8518f1a285842b6d863528269130cbe32
Paula Kunning <paulakunning@gmail.com>
2023-01-20 15:41:36 -0300

    deploy

--- src/models/Country.js
+++ src/models/Country.js
@@ -15,9 +15,6 @@ module.exports = (sequelize) => {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
-        validate: {
-          is: /^[a-z]+$/i,
-        }
       },
       flag: {
         type: DataTypes.STRING,
