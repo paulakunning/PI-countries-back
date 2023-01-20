578be19b91e3b76df9caa29a8f0593f325c0dcd1
Paula Kunning <paulakunning@gmail.com>
2023-01-20 16:38:51 -0300

    deploy

--- index.js
+++ index.js
@@ -19,11 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const { DB_PORT } = process.env
+const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(DB_PORT, () => {
-    console.log(`%s listening at ${DB_PORT}`); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
