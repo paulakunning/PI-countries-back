bb59948ea1180652787ed97fd7257f873ddfc05a
Paula Kunning <paulakunning@gmail.com>
2023-02-27 09:48:10 -0300

    deploy

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const cors = require('cors');
 
 require('./db.js');
 
@@ -12,6 +13,7 @@ server.name = 'API';
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cors());
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
